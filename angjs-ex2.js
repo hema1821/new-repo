1d75b502a8d43444b0a2da0282c0852e5000264e
hema1821 <shema_1821@yahoo.com>
2015-07-11 00:42:17 +0530

    first-commit

---
+++ angjs-ex2.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+<body>
+
+<div ng-app="" ng-init="points=[1,15,19,2,40]">
+
+<p>The third result is {{ points[2] }}</p>
+
+</div>
+
+</body>
+</html>
